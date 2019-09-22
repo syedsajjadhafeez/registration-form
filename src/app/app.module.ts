bc2161be986852f1741379bf73b137ea9933e041
syedsajjadhafeez <syedsajjadhafeez@gmail.com>
2019-09-22 07:36:26 -0700

    first commit in new branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactFormComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule
